48881ff0c429afeb97dbe714f56f191add46a352
Robert Moggach <rob@moggach.com>
2014-05-15 18:21:23 -0700

    dammit

--- mynk/configobj.py
+++ mynk/configobj.py
@@ -20,8 +20,9 @@ import sys
 from codecs import BOM_UTF8, BOM_UTF16, BOM_UTF16_BE, BOM_UTF16_LE
 
 import six
-from _version import __version__
+#from _version import __version__
 
+__version__ = '5.0.4'
 # imported lazily to avoid startup performance hit if it isn't used
 compiler = None
 
