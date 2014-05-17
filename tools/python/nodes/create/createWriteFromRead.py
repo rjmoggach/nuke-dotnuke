1283d280f00d8013046009b9f26007dd26a6c33c
Robert Moggach <rob@moggach.com>
2014-05-17 11:02:40 -0400

    damn tabs

--- tools/python/nodes/create/createWriteFromRead.py
+++ tools/python/nodes/create/createWriteFromRead.py
@@ -18,36 +18,36 @@ __menus__ = {
 }
 
 def createWriteFromRead(nodes=[]):
-	'''
-	function to create a read node
-	from a selected write node
-	'''
+  '''
+  function to create a read node
+  from a selected write node
+  '''
 
-	# if no nodes are defined look for selected nodes
-	if not nodes:
-		nodes = nuke.selectedNodes()
-	
-	# if nodes is still empty, nothing is selected
-	if not nodes:
-		nuke.message('ERROR: No node(s) selected.')
-		return
-	
-	for node in nodes:
-  	_class = node.Class()
-  	if _class == "Read":
-  		file = node.knob('file').getValue()
-  		proxy = node.knob('proxy').getValue()
-  		first = nuke.toNode('root').knob('first_frame').getValue()
-  		last = nuke.toNode('root').knob('last_frame').getValue()
-  		xpos = node.knob('xpos').getValue()
-  		ypos = int(node.knob('ypos').getValue()) + 40
-  		knobs = []
-  		fields = ('file','proxy','first','last','xpos','ypos')
-  		for entry in fields:
-  			if eval(entry) != '':
-  				knobs.append(entry)
-  				#knobs.append(str(eval(entry)))
-  		nuke.createNode('Write', string.join(knobs))	
-	return
+  # if no nodes are defined look for selected nodes
+  if not nodes:
+    nodes = nuke.selectedNodes()
+  
+  # if nodes is still empty, nothing is selected
+  if not nodes:
+    nuke.message('ERROR: No node(s) selected.')
+    return
+  
+  for node in nodes:
+    _class = node.Class()
+    if _class == "Read":
+      file = node.knob('file').getValue()
+      proxy = node.knob('proxy').getValue()
+      first = nuke.toNode('root').knob('first_frame').getValue()
+      last = nuke.toNode('root').knob('last_frame').getValue()
+      xpos = node.knob('xpos').getValue()
+      ypos = int(node.knob('ypos').getValue()) + 40
+      knobs = []
+      fields = ('file','proxy','first','last','xpos','ypos')
+      for entry in fields:
+        if eval(entry) != '':
+          knobs.append(entry)
+          #knobs.append(str(eval(entry)))
+      nuke.createNode('Write', string.join(knobs))  
+  return
 
 
