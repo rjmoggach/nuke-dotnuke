1142b988b14e0348e1467643752d8e94aef8229b
Robert Moggach <rob@moggach.com>
2014-09-22 19:43:16 -0700

    tweaks to kiss

--- tools/python/nodes/kiss.py
+++ tools/python/nodes/kiss.py
@@ -7,32 +7,38 @@ import nuke
 # 'Shift+F4' "+F4"
 
 __menus__ = {
-  'Tools/Nodes/Align/Kiss':  {
-    'cmd': 'toggle_node()',
+  'Tools/Nodes/Kiss Up':  {
+    'cmd': 'kiss_up()',
     'hotkey': 'F1',
     'icon': ''
   },
-  'Tools/Nodes/Align/Kiss Down':  {
-    'cmd': 'toggle_node_down()',
+  'Tools/Nodes/Kiss Down':  {
+    'cmd': 'kiss_down()',
     'hotkey': 'F2',
     'icon': ''
   }
 }
 
 
-# m.addCommand('ToggelNode...', ToggelNode, 'F1')
-# m.addCommand('ToggelDownNode...', Toggel_DownNode, 'F2')
 
-def check_proximity(node_xy, node_wh, sel_xy):
-  horiz = math.fabs(node_xy[0] - sel_xy[0]) <= node_wh[0]
-  vert = math.fabs(node_xy[0] - sel_xy[0]) <= node_wh[1]
+def check_proximity(sel_xywh, nearby_xywh, direction, proximity=[50,80]):
+  """
+  newer method to check if a node is below another node
+  Note: nuke coordinates are measured from top left corner for node and screen
+  """
+  horiz = nearby_xywh[0] - proximity[0] <= sel_xywh[0] + sel_xywh[2]/2 <= nearby_xywh[0] + nearby_xywh[2] + proximity[0]
+  if direction == 'up':
+    vert = nearby_xywh[1] + nearby_xywh[3] <= sel_xywh[1] <= nearby_xywh[1] + nearby_xywh[3] + proximity[1]
+  else:
+    vert = nearby_xywh[1] - proximity[1] <= sel_xywh[1] + sel_xywh[3] <= nearby_xywh[1]
   if horiz and vert: return True
   else: return False
 
-def toggle_node():
+
+def kiss_up(proximity=100):
   try:
     sel = nuke.selectedNode()
-    sel_xy = [sel.xpos(), sel.ypos()]
+    sel_xywh = [sel.xpos(), sel.ypos(), sel.screenWidth(), sel.screenHeight()]
   except:
     return
   all_nodes = nuke.allNodes()
@@ -49,9 +55,8 @@ def toggle_node():
     return
   for node in all_nodes:
     if not node is sel:
-      node_xy = [node.xpos(), node.ypos()]
-      node_wh = [node.screenWidth(), node.screenHeight()]
-      if check_proximity(node_xy, node_wh, sel_xy):
+      node_xywh = [node.xpos(), node.ypos(), node.screenWidth(), node.screenHeight()]
+      if check_proximity(sel_xywh, node_xywh, 'up'):
         for input_num in connected:
           if sel.input(input_num).name() == node.name():
             related = True
@@ -64,10 +69,10 @@ def toggle_node():
           return
 
 
-def toggle_node_down():
+def kiss_down(proximity=100):
   try:
     sel = nuke.selectedNode()
-    sel_xy = [sel.xpos(), sel.ypos()]
+    sel_xywh = [sel.xpos(), sel.ypos(), sel.screenWidth(), sel.screenHeight()]
   except:
     return
   all_nodes = nuke.allNodes()
@@ -78,9 +83,8 @@ def toggle_node_down():
   all_nodes = nuke.allNodes()
   for node in all_nodes:
     if not node is sel:
-      node_xy = [node.xpos(), node.ypos()]
-      node_wh = [node.screenWidth(), node.screenHeight()]
-      if check_proximity(node_xy, node_wh, sel_xy):
+      node_xywh = [node.xpos(), node.ypos(), node.screenWidth(), node.screenHeight()]
+      if check_proximity(sel_xywh, node_xywh, 'down'):
         max_inputs = node.maxInputs()
         for m in range(max_inputs):
           if node.input(m) is sel:
